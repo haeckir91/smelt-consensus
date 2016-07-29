6b33953c46964c16087910fbf127ea7baa6c2d01
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-07-29 11:55:35 +0200

    scripts: changed run script to work with bench-framework

--- bench/run_all_benchmarks.sh
+++ bench/run_all_benchmarks.sh
@@ -1,29 +1,54 @@
 #!/bin/bash
-#All non libsync benchmarks
+
+function error() {
+	echo $1
+	exit 1
+}
+
+while [[ $RUN -eq 1 ]]; do
+
+	echo "checking argument .. $1"
+
+	case "$1" in
+
+		"-d")
+			echo "Executing in debug mode"
+			DEBUG=1
+			shift
+			;;
+
+		*)
+			RUN=0 # Stop loop execution
+			echo "No more arguments to parse .. "
+
+	esac
+done
 
 declare -a arr2=(config_files/config_r815_8_smlt \
                  config_files/config_r815_12_smlt \
                  config_files/config_r815_16_smlt \
                  config_files/config_r815_20_smlt \
-                 config_files/config_r815_24_smlt \ 
+                 config_files/config_r815_24_smlt \
                  config_files/config_r815_28_smlt )
 
 # run shared memory hybrid
-<<comment
-for i in {0..3}
-do
-    for j in "${arr[@]}"
-    do
-        ./start_bench $i 4 "$j"
-    done
-done
-comment
+# for i in {0..3}
+# do
+#     for j in "${arr[@]}"
+#     do
+#         ./start_bench $i 4 "$j"
+#     done
+# done
 
-#run single tier
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
+# run single tier
 for j in "${arr2[@]}"
 do
-    ./start_bench 0 6 "$j" 
-    ./start_bench_smelt 0 6 "$j" 
-    ./start_bench 2 6 "$j" 
-    ./start_bench_smelt 2 6 "$j" 
+    ./start_bench 0 6 "$j"       || error "Failed to execute ./start bench 0 6"
+    ./start_bench_smelt 0 6 "$j" || error "Failed to execute ./start bench_smelt 0 6"
+    ./start_bench 2 6 "$j"       || error "Failed to execute ./start bench 2 6"
+    ./start_bench_smelt 2 6 "$j" || error "Failed to execute ./start bench_smelt 2 6"
 done
+
+exit 0
